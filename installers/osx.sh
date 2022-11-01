56126011a2c7de066383a2831206f25fcf1b289e
Greg Gaston <gaston.greg@bcg.com>
2022-11-01 11:56:09 -0400

    add changes that I gorgot about

--- installers/osx.sh
+++ installers/osx.sh
@@ -6,11 +6,18 @@
 if [ $(type brew > /dev/null; echo $?) -ne 0 ] ; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+  touch ~/.zsh/env
+  chmod 666 ~/.zsh/env
   echo "export PATH=\$PATH:/opt/homebrew/bin" >> ~/.zsh/env
 fi
 
+source ~/.zshrc
+
 # Languages
-brew install go scala python
+brew install go scala sbt python
+echo "export PATH=\$PATH:\$(go env GOPATH)/bin" >> ~/.zsh/env
+
+go install golang.org/x/tools/cmd/goimports@latest
 
 
 # Tools
